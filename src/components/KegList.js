d7126b5347707a42c01b679543c9706ebc68ad50
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-03 10:27:23 -0700

    add .map method for Keg properties when clicked in KegList.

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,10 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Keg from "./Keg";
 
-function KegList() {
+function KegList(props) {
   return (
-    <Keg />
+    <React.Fragment>
+      <hr />
+      {props.kegList.map((keg) => 
+        <Keg 
+          whenKegClicked = { props.onKegSelection }
+          name = {keg.name}
+          brand = {keg.brand}
+          price = {keg.price}
+          abv = {keg.abv}
+          wholeKeg = {keg.wholeKeg}
+          id = {keg.id}
+          key = {keg.id}
+        />
+      )}
+    </React.Fragment>
   ); 
+}
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
 }
 
 export default TicketList;
