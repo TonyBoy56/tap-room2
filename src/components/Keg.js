f6848223202e1d264b23a6dcd7f192a5fdea63ed
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-03 09:55:12 -0700

    add proptypes to Keg.js.

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,13 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Keg() {
+function Keg(props) {
   return (
     <React.Fragment>
-      <p><em>Firebase entries not saving!</em></p>
+      <div onClick = {() => props.whenKegClicked(props.id)}>
+
+      </div>
       <hr/>
     </React.Fragment>
   );
+}
+
+Keg.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired
+  price: PropTypes.string.isRequired,
+  abv: PropTypes.string.isRequired,
+  wholeKeg: PropTypes.number.isRequired,
+  id: PropTypes.string,
+  whenKegClicked: PropTypes.func
 }
 
 export default Keg;
