360da5fe90f205a302b4caf491602792ce71202e
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-03 12:44:44 -0700

    add KegDetail method with React.Frag data. Include KegDetail prop.

---
+++ src/components/KegDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function KegDetail(props) {
+  const { keg } = props;
+
+  return (
+    <React.Fragment>
+      <h1>Keg Details</h1>
+      <h3>{keg.name} - {keg.brand}</h3>
+      <h5>${keg.price} /pint</h5>
+      <h5>{keg.abv}% ABV</h5>
+      <h5>Quantity: {keg.wholeKeg} pints</h5>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+KegDetail.propTypes = {
+  keg: PropTypes.object
+};
+
+export default KegDetail;
