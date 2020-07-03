bff98c55abdbfbc062306ba34cccadd4f171c51f
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-03 09:33:16 -0700

    allocate previous App.js data to Keg, export Keg.

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,8 +1,13 @@
 import React from "react";
-import ProTypes from "prop-types";
+import PropTypes from "prop-types";
 
 function Keg() {
   return (
-    
+    <React.Fragment>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </React.Fragment>
   );
 }
+
+export default Keg;
