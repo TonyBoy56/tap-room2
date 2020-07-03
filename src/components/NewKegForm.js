9c07c0258f49aa3bce914e1757ba9a70162ca078
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-03 11:06:43 -0700

    call Resuable form within React.Fragment in NewKegForm.

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { v4 } from "uuid";
 import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
 
 function NewKegForm(props) {
   
@@ -19,7 +20,15 @@ function NewKegForm(props) {
 
   return (
     <React.Fragment>
-      
+      <ReusableForm
+        formSubmissionHandler = { handleNewKegFormSubmission }
+        buttonText = "Add new Keg" />
     </React.Fragment>
   );
 }
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+}
+
+export default NewKegForm;
