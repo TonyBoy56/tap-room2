b6f83a092a91964eb363dccfc72251b407da13cf
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-03 10:11:28 -0700

    add propTypes from Keg to div Onclick method.

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -5,7 +5,10 @@ function Keg(props) {
   return (
     <React.Fragment>
       <div onClick = {() => props.whenKegClicked(props.id)}>
-
+        <h3>{props.name} - {props.brand}</h3>
+        <h5>{props.price}</h5>
+        <h5>{props.abv}</h5>
+        <h5>{props.wholeKeg} pints</h5>
       </div>
       <hr/>
     </React.Fragment>
@@ -14,7 +17,7 @@ function Keg(props) {
 
 Keg.propTypes = {
   name: PropTypes.string.isRequired,
-  brand: PropTypes.string.isRequired
+  brand: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
   abv: PropTypes.string.isRequired,
   wholeKeg: PropTypes.number.isRequired,
