4ffe3548660dd923d422d01c5344d2b00ffb9e98
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-02 19:29:45 -0700

    add components dir with App.js.

--- README.md
+++ README.md
@@ -61,6 +61,9 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/ad
 
 ### Deployment
 
+### Diagrams
+![App Layout](./diags/diag.drawio.svg)
+
 This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
 
 ### `npm run build` fails to minify
---
+++ components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <React.Fragment>
+        <h1>The Tap Room</h1>
+        <p><em>Firebase entries not saving!</em></p>
+        <hr/>
+      </React.Fragment>
+    </div>
+  );
+}
+
+export default App;
