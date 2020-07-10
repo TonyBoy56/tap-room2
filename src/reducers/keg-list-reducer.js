f962fc87bee837f2d56eb37cf40c11bfa63e55d8
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-10 10:50:25 -0700

    add new keg data test now passes.

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, price, abv, quantity, id } = action;
+  switch (action.type) {
+  case 'ADD_KEG':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand, 
+        price: price,
+        abv: abv,
+        quantity: quantity,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 };
